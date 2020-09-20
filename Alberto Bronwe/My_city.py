3133cb7f474f90c288326f7e6fc7d277fa5ddf40
Alberto Browne <aebrowne@gmail.com>
2020-09-20 17:16:07 +0200

    Alberto Bronwe_My City

---
+++ Alberto Bronwe/My_city.py
@@ -0,0 +1,27 @@
+City_area = 15403
+City_population = 7112808
+
+# Let's assign the result of an operation
+City_density = City_population / City_area
+Factor_density = City_density / City_area
+
+# Let's print the information
+print('1. Country:')
+print('Chile')
+print('2. City:')
+print('Santiago')
+print('Latitude: -33.4372, Longitude: -70.6506 \n\n')
+print('3. City Area:')
+print('15.403 km2')
+print('4. Population:')
+print('7.112.808 people \n\n')
+
+# Let's print out the result
+print('5. City Density:')
+print(City_density)
+
+# Let's print out an other result
+print('6. Factor Density:')
+print(Factor_density)
+print('........ \n\n')
+print('Thanks')
